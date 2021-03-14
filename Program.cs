4ce5fd21afbb547548ef85cf73df1eb4dfa583a7
Vinicius <viniciusgomeshonorio@hotmail.com>
2021-03-14 15:33:15 -0300

    Exercicio GRITADOR

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio_GRITADOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textomodificado;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Escreva Qualquer coisa, Sera automaticamente trocado por letras maiusculas:");
+            Console.ResetColor();
+
+            textomodificado= Convert.ToString(Console.ReadLine());
+            Console.WriteLine($"{textomodificado.ToUpper()}");
+        }   
+    }
+}
